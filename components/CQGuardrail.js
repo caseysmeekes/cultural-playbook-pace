412109b1f5978278a3153333c092add7bd72d1be
caseysmeekes <casey.smeekes@gmail.com>
2026-09-09 08:15:51 +1200

    PACE V2C - Add CQ personalisation tools

--- components/CQGuardrail.js
+++ components/CQGuardrail.js
@@ -1,1 +1,2 @@
 export default function CQGuardrail(){return <div className="cq-guardrail"><b>PACE PRINCIPLE</b><span>Culture gives you a hypothesis. The individual gives you the answer.</span></div>}
+
