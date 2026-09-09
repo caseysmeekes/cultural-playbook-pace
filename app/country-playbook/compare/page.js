4017b8eb8be8b42571498664799d268b5add8a75
caseysmeekes <casey.smeekes@gmail.com>
2026-09-09 21:04:59 +1200

    PACE 3D - Build Country Playbook comparison

---
+++ app/country-playbook/compare/page.js
@@ -0,0 +1,10 @@
+import CountryPlaybookComparison from '../../../components/CountryPlaybookComparison'
+
+export const metadata = {
+  title: 'Country Playbook Comparison | The Cultural Playbook',
+  description: 'Compare how your business approach may need to change between two countries.'
+}
+
+export default function CountryPlaybookComparePage() {
+  return <CountryPlaybookComparison />
+}
