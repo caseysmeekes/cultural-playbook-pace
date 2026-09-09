e1ba1e3d6b5cd4f78d260bf5ecb89072e6ba1e33
caseysmeekes <casey.smeekes@gmail.com>
2026-09-09 20:52:13 +1200

    PACE 3A - Build Country Playbook foundation

---
+++ app/country-playbook/page.js
@@ -0,0 +1,10 @@
+import CountryPlaybook from '../../components/CountryPlaybook'
+
+export const metadata = {
+  title: 'Country Playbook | The Cultural Playbook',
+  description: 'An interactive business and cultural country playbook.'
+}
+
+export default function CountryPlaybookPage() {
+  return <CountryPlaybook />
+}
